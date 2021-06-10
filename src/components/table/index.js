7ec8db47fc1da37aee5f050aacaa93873e6d720a
Pariskrit Mokta <moktanpariskrit@gmail.com>
2021-06-10 10:23:08 +0545

    fixed expand arrow bug

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -37,6 +37,7 @@ function Table({
   useEffect(() => {
     setData(rowData);
   }, [rowData]);
+
   return (
     <div className="table" style={{ width: width + "%" }}>
       <div
@@ -62,7 +63,7 @@ function Table({
               key={row.id}
             >
               <div className="table__body-row" style={{ gridTemplateColumns }}>
-                {isNestedTable && row.rowContent ? (
+                {row?.subCategories?.length > 0 ? (
                   <div
                     onClick={() => toggleRowExpand(row.id)}
                     className="toggle-icon"
