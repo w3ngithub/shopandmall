a327d85691cf15de51b726578d7480032f7778d9
Sagar TS <shrestha.sagar@webexpertsnepal.com>
2021-06-11 07:28:46 +0000

    Resolve "Create card component"

--- src/components/mall/Mall.js
+++ src/components/mall/Mall.js
@@ -1,157 +1,18 @@
 import React from "react";
-import classes from "../Dashboard/dashboard.module.css";
-import NoImage from "../../image/No_Image_Available.jpg";
-import { useHistory, useLocation } from "react-router-dom";
-import { fireStore, storage } from "../../firebase/config";
-import { IoMdCloseCircle } from "react-icons/io";
+import classes from "./mall.module.css";
+import MallCardComponent from "../mallCardComponent/MallCardComponent";
 
 const Mall = ({ docs }) => {
-  console.log("malls", docs);
-  const history = useHistory();
-  const location = useLocation();
+    console.log("malls", docs);
 
-  return (
-    <div className={classes.container}>
-      {docs?.map((doc, ind) =>
-        location.pathname === "/admin/dashboard" ||
-        location.pathname === "/" ? (
-          ind <= 2 && (
-            <div
-              className={classes.wrapper}
-              key={doc.id}
-              onClick={() => {
-                location.pathname.split("/")[1] === "admin"
-                  ? history.push("/admin/malls/" + doc.id.replace(" ", "_"))
-                  : history.push("/malls/" + doc.id.replace(" ", "_"));
-              }}
-            >
-              {doc.mallImage ? (
-                <div className={classes.imageContainer}>
-                  {location.pathname === "/admin/dashboard" && (
-                    <div
-                      className={classes.closeIcon}
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        fireStore
-                          .collection("Shopping Mall")
-                          .doc(doc.mallName)
-                          .delete()
-                          .then(() => console.log("DELETED Successfully"))
-                          .catch((error) => console.log("Error deleting mall"));
-                        let storageRef = storage.ref();
-                        let mallImageDel = storageRef.child(
-                          doc.mallImage.imageName
-                        );
-
-                        //Shop Images
-                        doc.shops.map((shop) =>
-                          shop.shopImages.map((s) =>
-                            storageRef
-                              .child(s.ImageName)
-                              .delete()
-                              .then(() => "Images Deleted SuccessFUlly")
-                              .catch((err) => "Images Not Deleted")
-                          )
-                        );
-
-                        //Deleting Images
-                        mallImageDel
-                          .delete()
-                          .then(() => "Images Deleted SuccessFUlly")
-                          .catch((err) => "Images Not Deleted");
-                      }}
-                    >
-                      <IoMdCloseCircle />
-                    </div>
-                  )}
-                  <div>
-                    <img
-                      className={classes.image}
-                      src={doc.mallImage.imageUrl}
-                      alt="images"
-                    />
-                  </div>
-                </div>
-              ) : (
-                <div className={classes.imageContainer}>
-                  <img className={classes.image} src={NoImage} alt="images" />
-                </div>
-              )}
-              <p className={classes.title}>{doc.mallName}</p>
-              <p>({doc.mallAddress})</p>
-            </div>
-          )
-        ) : (
-          <div
-            className={classes.wrapper}
-            key={doc.id}
-            onClick={() =>
-              location.pathname.split("/")[1] !== "admin"
-                ? history.push("/malls/" + doc.id.replace(" ", "_"))
-                : history.push("/admin/malls/" + doc.id.replace(" ", "_"))
-            }
-          >
-            {doc.mallImage ? (
-              <div className={classes.imageContainer}>
-                {location.pathname !== "/malls" && (
-                  <div
-                    className={classes.closeIcon}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      fireStore
-                        .collection("Shopping Mall")
-                        .doc(doc.mallName)
-                        .delete()
-                        .then(() => console.log("DELETED Successfully"))
-                        .catch((error) => console.log("Error deleting mall"));
-                      let storageRef = storage.ref();
-                      let mallImageDel = storageRef.child(
-                        doc.mallImage.imageName
-                      );
-
-                      //Shop Images
-                      doc &&
-                        doc.shops.map((shop) =>
-                          shop.shopImages.map((s) =>
-                            storageRef
-                              .child(s.ImageName)
-                              .delete()
-                              .then(() => "Images Deleted SuccessFUlly")
-                              .catch((err) => "Images Not Deleted")
-                          )
-                        );
-
-                      //Deleting Images
-                      mallImageDel
-                        .delete()
-                        .then(() => "Images Deleted SuccessFUlly")
-                        .catch((err) => "Images Not Deleted");
-                    }}
-                  >
-                    <IoMdCloseCircle />
-                  </div>
-                )}
-
-                <div>
-                  <img
-                    className={classes.image}
-                    src={doc.mallImage.imageUrl}
-                    alt="images"
-                  />
-                </div>
-              </div>
-            ) : (
-              <div className={classes.imageContainer}>
-                <img className={classes.image} src={NoImage} alt="images" />
-              </div>
+    return (
+        <div className={classes.container}>
+            {docs?.map(
+                (doc, ind) =>
+                    ind <= 2 && <MallCardComponent key={doc.id} doc={doc} />
             )}
-            <p className={classes.title}>{doc.mallName}</p>
-            <p>({doc.mallAddress})</p>
-          </div>
-        )
-      )}
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Mall;
--- src/pages/allMalls.js
+++ src/pages/allMalls.js
@@ -6,72 +6,72 @@ import classes from "../styles/allMalls.module.css";
 import { useHistory, useLocation } from "react-router-dom";
 
 const AllMalls = () => {
-  const [search, setSearch] = useState("");
+    const [search, setSearch] = useState("");
 
-  let { docs } = useFirestore("Shopping Mall");
+    let { docs } = useFirestore("Shopping Mall");
 
-  //Pagination
-  const [currentPage, setCurrentPage] = useState(1);
-  const [mallsPerPage] = useState(3);
+    //Pagination
+    const [currentPage, setCurrentPage] = useState(1);
+    const [mallsPerPage] = useState(3);
 
-  const indexOfLastMall = currentPage * mallsPerPage;
-  const indexOfFirstMall = indexOfLastMall - mallsPerPage;
-  const currentMalls = docs.slice(indexOfFirstMall, indexOfLastMall);
+    const indexOfLastMall = currentPage * mallsPerPage;
+    const indexOfFirstMall = indexOfLastMall - mallsPerPage;
+    const currentMalls = docs.slice(indexOfFirstMall, indexOfLastMall);
 
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  // ------------
+    // ------------
 
-  const location = useLocation();
-  const history = useHistory();
+    const location = useLocation();
+    const history = useHistory();
 
-  const filter = (e) => {
-    setSearch(e.target.value);
-  };
+    const filter = (e) => {
+        setSearch(e.target.value);
+    };
 
-  if (search) {
-    docs = docs.filter((doc) =>
-      doc.mallName.toLowerCase().includes(search.toLowerCase())
+    if (search) {
+        docs = docs.filter((doc) =>
+            doc.mallName.toLowerCase().includes(search.toLowerCase())
+        );
+    }
+    return (
+        <div className={classes.main}>
+            {location.pathname === "/admin/malls" ? (
+                <button
+                    className={classes.addBtn}
+                    onClick={() => history.push("/admin/newMall")}
+                >
+                    Add New Mall
+                </button>
+            ) : (
+                <div className={classes.search}>
+                    <input
+                        className={classes.searchBar}
+                        type="text"
+                        placeholder="Search..."
+                        onChange={filter}
+                    />
+                </div>
+            )}
+            <div className={classes.mallContainer}>
+                <div className={classes.header}>
+                    <h4 className={classes.heading}>Malls</h4>
+                </div>
+                {docs?.length !== 0 ? (
+                    <>
+                        <Mall docs={currentMalls} />
+                        <Pagination
+                            mallsPerPage={mallsPerPage}
+                            docs={docs.length}
+                            paginate={paginate}
+                        />
+                    </>
+                ) : (
+                    <h3>No Malls Added Yet</h3>
+                )}
+            </div>
+        </div>
     );
-  }
-  return (
-    <div className={classes.main}>
-      {location.pathname === "/admin/malls" ? (
-        <button
-          className={classes.addBtn}
-          onClick={() => history.push("/admin/newMall")}
-        >
-          Add New Mall
-        </button>
-      ) : (
-        <div className={classes.search}>
-          <input
-            className={classes.searchBar}
-            type="text"
-            placeholder="Search..."
-            onChange={filter}
-          />
-        </div>
-      )}
-      <div className={classes.mallContainer}>
-        <div className={classes.header}>
-          <h4 className={classes.heading}>Malls</h4>
-        </div>
-        {docs?.length !== 0 ? (
-          <>
-            <Mall docs={currentMalls} />
-            <Pagination
-              mallsPerPage={mallsPerPage}
-              docs={docs.length}
-              paginate={paginate}
-            />
-          </>
-        ) : (
-          <h3>No Malls Added Yet</h3>
-        )}
-      </div>
-    </div>
-  );
 };
 
 export default AllMalls;
