8ed7a1c375ea1e6d85a6f61380ada74c09d37e80
SagarTS <sagartshrestha@gmail.com>
2021-06-14 13:05:48 +0545

    Fixed Shop Category Modal Design

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,11 @@
 import "./button.css";
 
 const Button = ({ onClick, type, children }) => {
-  return (
-    <button className="button" type={type} onClick={onClick && onClick}>
-      {children}
-    </button>
-  );
+    return (
+        <button className="button" type={type} onClick={onClick && onClick}>
+            {children}
+        </button>
+    );
 };
 
 export default Button;
