c5bfc250be0b26506c375451fb00f1a58387899a
Pariskrit Mokta <moktanpariskrit@gmail.com>
2021-07-08 16:08:30 +0545

    created custom hook for filtering mall and shops

---
+++ src/hooks/useFilterMallAndShops.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export const useFilterMallAndShops = (malls, isShopCategorySelected) => {
+  const location = useLocation();
+  const [filteredMalls, setFilteredMalls] = useState([]);
+
+  const handleFilterShops = () => {
+    const selectedCategory = location.pathname.split("/")[3];
+    const selectedSubCategory = location.pathname.split("/")[4];
+    let filtering = [];
+
+    malls.forEach((mall) => {
+      const shopsWithTheCategory = mall.shops.filter(
+        (shop) => shop.category === selectedCategory
+      );
+      const shopsWithTheCategoryAndSubCategory = mall.shops.filter(
+        (shop) =>
+          shop.category === selectedCategory &&
+          shop.subCategory === selectedSubCategory
+      );
+
+      if (shopsWithTheCategory.length > 0 && !Boolean(selectedSubCategory)) {
+        filtering = [...filtering, { ...mall, shops: shopsWithTheCategory }];
+      }
+
+      if (shopsWithTheCategoryAndSubCategory.length > 0) {
+        filtering = [
+          ...filtering,
+          { ...mall, shops: shopsWithTheCategoryAndSubCategory },
+        ];
+      }
+    });
+
+    setFilteredMalls(filtering);
+  };
+
+  useEffect(() => {
+    if (isShopCategorySelected) {
+      handleFilterShops();
+    } else {
+      setFilteredMalls(malls);
+    }
+  }, [location.pathname, malls]);
+
+  return { filteredMalls };
+};
