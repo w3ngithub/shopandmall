c67e6c4600d5e07573dfa2de555c4a60899e97c7
Pariskrit Mokta <moktanpariskrit@gmail.com>
2021-07-29 13:15:15 +0545

    validated for deleting category/subcategory

--- src/components/single/AddShopCategoryModal.js
+++ src/components/single/AddShopCategoryModal.js
@@ -4,116 +4,100 @@ import { IoClose } from "react-icons/io5";
 import { IoIosAddCircle } from "react-icons/io";
 
 const ShopCategoryModal = ({
-    categoryError,
-    subCategoryError,
-    category,
-    setCategory,
-    subCategories,
-    setSubCategories,
-    onSubmit,
-    setShowModal,
+  categoryError,
+  subCategoryError,
+  category,
+  setCategory,
+  subCategories,
+  setSubCategories,
+  onSubmit,
+  setShowModal,
+  removeSubCategory,
 }) => {
-    return (
-        <>
-            <div
-                className={classes.modalBackground}
-                onClick={setShowModal}
-            ></div>
-            <div className={classes.modal}>
-                <div className={classes.header}>
-                    <h3 className={classes.title}>Add New Category</h3>
-                    <span onClick={setShowModal}>
-                        <IoClose className={classes.closeIcon} />
-                    </span>
-                </div>
-                <div className={classes.line}></div>
+  return (
+    <>
+      <div className={classes.modalBackground} onClick={setShowModal}></div>
+      <div className={classes.modal}>
+        <div className={classes.header}>
+          <h3 className={classes.title}>Add New Category</h3>
+          <span onClick={setShowModal}>
+            <IoClose className={classes.closeIcon} />
+          </span>
+        </div>
+        <div className={classes.line}></div>
 
-                <form onSubmit={onSubmit} className={classes.form}>
-                    <input
-                        type="text"
-                        className={style.input}
-                        placeholder="Category Name"
-                        value={category.category}
-                        onChange={(e) =>
-                            setCategory({
+        <form onSubmit={onSubmit} className={classes.form}>
+          <input
+            type="text"
+            className={style.input}
+            placeholder="Category Name"
+            value={category.category}
+            onChange={(e) =>
+              setCategory({
+                ...category,
+                category: e.target.value,
+              })
+            }
+          />
+          {categoryError.invalid && (
+            <p className={classes.categoryError}>*{categoryError.message}</p>
+          )}
+          {subCategories.length > 0 &&
+            subCategories.map(({ id, subCategory }) => (
+              <div key={id}>
+                <div className={classes.form__subcategory}>
+                  <input
+                    type="text"
+                    className={style.input}
+                    placeholder="Sub-Category Name"
+                    value={subCategory}
+                    onChange={(e) =>
+                      setSubCategories([
+                        ...subCategories.map((category) =>
+                          category.id === id
+                            ? {
                                 ...category,
-                                category: e.target.value,
-                            })
-                        }
-                    />
-                    {categoryError.invalid && (
-                        <p className={classes.categoryError}>
-                            *{categoryError.message}
-                        </p>
+                                subCategory: e.target.value,
+                              }
+                            : category
+                        ),
+                      ])
+                    }
+                  />
+
+                  <span onClick={() => removeSubCategory(subCategory)}>
+                    <IoClose className={style.closeIcon} />
+                  </span>
+                  <div className={classes.subCategoryError}>
+                    {subCategoryError.id === id && (
+                      <p>*{subCategoryError.message}</p>
                     )}
-                    {subCategories.length > 0 &&
-                        subCategories.map(({ id, subCategory }) => (
-                            <div key={id}>
-                                <div className={classes.form__subcategory}>
-                                    <input
-                                        type="text"
-                                        className={style.input}
-                                        placeholder="Sub-Category Name"
-                                        value={subCategory}
-                                        onChange={(e) =>
-                                            setSubCategories([
-                                                ...subCategories.map(
-                                                    (category) =>
-                                                        category.id === id
-                                                            ? {
-                                                                  ...category,
-                                                                  subCategory:
-                                                                      e.target
-                                                                          .value,
-                                                              }
-                                                            : category
-                                                ),
-                                            ])
-                                        }
-                                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+          <p
+            className={classes.label}
+            onClick={() =>
+              setSubCategories([
+                ...subCategories,
+                { id: Date.now(), subCategory: "" },
+              ])
+            }
+          >
+            Add Sub Category
+            <span>
+              <IoIosAddCircle className={classes.addIcon} />
+            </span>
+          </p>
 
-                                    <span
-                                        onClick={() =>
-                                            setSubCategories(
-                                                subCategories.filter(
-                                                    (category) =>
-                                                        category.id !== id
-                                                )
-                                            )
-                                        }
-                                    >
-                                        <IoClose className={style.closeIcon} />
-                                    </span>
-                                    <div className={classes.subCategoryError}>
-                                        {subCategoryError.id === id && (
-                                            <p>*{subCategoryError.message}</p>
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    <p
-                        className={classes.label}
-                        onClick={() =>
-                            setSubCategories([
-                                ...subCategories,
-                                { id: Date.now(), subCategory: "" },
-                            ])
-                        }
-                    >
-                        Add Sub Category
-                        <span>
-                            <IoIosAddCircle className={classes.addIcon} />
-                        </span>
-                    </p>
-
-                    <button className={classes.submitBtn} type="submit">
-                        Save
-                    </button>
-                </form>
-            </div>
-        </>
-    );
+          <button className={classes.submitBtn} type="submit">
+            Save
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default ShopCategoryModal;
