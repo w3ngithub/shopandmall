1b1ca601d38e6cde130c29b7d44620446e990fd3
Pariskrit Mokta <moktanpariskrit@gmail.com>
2021-08-02 10:59:38 +0545

    added .env file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,14 @@ import "firebase/firestore";
 import firebase from "firebase/app";
 
 // Your web app's Firebase configuration
+
 var firebaseConfig = {
-  apiKey: "AIzaSyAtQ9jVPG8-vr-PYJsRbZojAQhDc7RWlik",
-  authDomain: "shopping-mall-67399.firebaseapp.com",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: "shopping-mall-67399",
-  storageBucket: "shopping-mall-67399.appspot.com",
-  messagingSenderId: "26390021620",
-  appId: "1:26390021620:web:bc1f2c765282cd1e1765a1",
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
